df6d55ed08f409a6f398e7b1b382ba69d8607389
Felipe Passos <passos.fe@gmail.com>
2019-11-27 20:10:49 -0300

    added header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 
 import Routes from "./routes";
 import GlobalStyles from "./styles/global";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-// import { Container } from './styles';
+import { ProductList } from "./styles";
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
